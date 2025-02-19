e3e3973e91b116316746663c5ddadde3fc48d83e
Colton Wollinger <cwoll1home@gmail.com>
2025-02-19 17:37:02 -0500

    I hate my code

--- newsrc/web_server.py
+++ newsrc/web_server.py
@@ -5,31 +5,42 @@ import utils
 
 app = Flask(__name__)
 
+INFO, WARN, ERROR, CRITICAL, VERBOSE, VERBOSEX = utils.get_loglevels()
+
 users = {
     "colton": messages.User("colton", "abc123")
 }
+sessions = {}
 guilds = {}
+
+def get_session(token):
+    return users[ sessions[ token ] ]
 
 @app.route("/api/logon", methods=["POST"])
 def login():
-    data = request.json
-    uname = data["username"]
+    data =   request.json
+    uname =  data["username"]
     passwd = data["password"]
 
     users[uname].authenticate(passwd)
-    if users[uname].token is None:
+    token = users[uname].token
+    if token is None:
         return "No", 404
 
-    r = make_response(users[uname].token)
-    r.set_cookie("token", users[uname].token, secure=True, samesite="None")
+    r = make_response(token)
+    r.set_cookie("token", token, secure=True, samesite="None")
+    sessions[token] = uname
+    utils.log(INFO, f"login: usr={uname}, pass={passwd}, tok={token}")
     return r
 
 @app.route("/api/newuser", methods=["POST"])
 def newuser():
-    data = request.json
-    uname = data["username"]
+    data =   request.json
+    uname =  data["username"]
     passwd = data["password"]
     users[uname] = messages.User(uname, passwd)
+
+    utils.log(INFO, f"new user: {uname}")
 
     return "OK"
 
@@ -41,18 +52,37 @@ def newguild():
     cid = utils.generate_id()
     guilds[cid] = messages.Guild(cid)
 
+    utils.log(INFO, f"newguild: req={data}, cid={cid}, out={guilds[cid]}")
+
     return cid
 
 @app.route("/api/newchannel", methods=["POST"])
 def addchannel():
-    data = request.json
+    data =  request.json
     token = data["token"]
     guild = data["guildid"]
-    name = data["name"]
+    name =  data["name"]
 
     cid = utils.generate_id()
     guilds[guild].channels[cid] = messages.Channel(name, cid)
 
+    utils.log(INFO, f"newchannel: req={data}.tok={token}.guild={guild}\
+        .name={name}, cid={cid}, out={guilds[guild].channels[cid]}")
+
     return cid
+
+@app.route("/api/sendmsg", methods=["POST"])
+def addmsg():
+    data = request.json
+    token = data["token"]
+    channel = data["channel"]
+    guild = data["guild"]
+    content = data["content"]
+
+    uname = get_session(token)
+    message = messages.Message(uname, content, "")
+
+    guilds[guild].channels[channel].add_message(message)
+    return message.cid
 
 app.run()
